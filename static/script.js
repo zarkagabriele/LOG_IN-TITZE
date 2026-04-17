51bed99785b50ebb4a278f1c77c181c77ca1d672
gabo nabo <zarka.gabriele@iisgalvanimi.edu.it>
2026-04-17 07:43:28 +0000

    da finire js

---
+++ static/script.js
@@ -0,0 +1,8 @@
+async function controllaCredenziali() {
+    const nome = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    if (!username || !password ) return alert("Scrivi uno username e password!");
+    const res = await fetch(`/login?username=${username} & password = $("password)`);
+    const dati = await res.json();
+    document.getElementById('risposta-saluto').innerText = dati.messaggio;
+}
