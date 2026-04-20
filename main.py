1a668f6841b0962f477e5ef51ed4c35c8f41ad9d
gabo nabo <zarka.gabriele@iisgalvanimi.edu.it>
2026-04-20 09:20:32 +0000

    login con post

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
 @app.get("/")
 def home():
-    return FileResponse('static/index.html')
+    return FileResponse("static/index.html")
 
-@app.get("/login")
-def controlla(username: str, password: str):
-    if username == "admin" and password == "xxx123":
-        risposta = {"messaggio": "le credenziali sono corrette"}
-    else:
-        risposta = {"messaggio": "le credenziali sono sbagliate"}
-    return (risposta)
+
+class Login(BaseModel):
+    username: str
+    password: str
+
+
+@app.post("/login")
+def controlla(dati: Login):
+    if dati.username == "admin" and dati.password == "xxx123##":
+        return {"messaggio": "le credenziali sono corrette"}
+
+    return {"messaggio": "le credenziali sono sbagliate"}
--- static/script.js
+++ static/script.js
@@ -2,12 +2,17 @@ async function controllaCredenziali() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    if (!username || !password) {
-        alert("Scrivi username e password!");
-        return;
-    }
+    const res = await fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    });
 
-    const res = await fetch(`/login?username=${username}&password=${password}`);
     const dati = await res.json();
 
     document.getElementById('risultato').innerText = dati.messaggio;
