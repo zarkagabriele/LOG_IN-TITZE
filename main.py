f270db8fd06ad7812e423499f4882eab3973069f
gabo nabo <zarka.gabriele@iisgalvanimi.edu.it>
2026-04-23 10:34:22 +0000

    log in con get

--- main.py
+++ main.py
@@ -15,6 +15,6 @@ def home():
 @app.get("/login")
 def controlla(username: str, password: str):
     if username == "admin" and password == "xxx123":
-        return {"messaggio": "le credenziali sono corrette"}
+        return {"messaggio": "1. le credenziali sono corrette"}
 
-    return {"messaggio": "le credenziali sono sbagliate"}
+    return {"messaggio": "0. le credenziali sono sbagliate"}
